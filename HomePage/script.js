0707d2051668f27346a7199c26e76b0dc4bdcb8d
yeshbun <yesh_bun@hotmail.com>
2022-05-14 10:29:46 +0400

    Fourth Commit

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,4 +1,4 @@
-$('#recipeCarousel').carousel({
+/*$('#recipeCarousel').carousel({
     interval: 10000
   })
   
@@ -18,38 +18,5 @@ $('#recipeCarousel').carousel({
           
           next.children(':first-child').clone().appendTo($(this));
         }
-  });
+  }); */
 
-  let modal;
-document.addEventListener("click", (e) => {
-  if (e.target.className === "modal-open") {
-    modal = document.getElementById(e.target.dataset.id);
-    openModal(modal);
-  } else if (e.target.className === "modal-close") {
-    closeModal(modal);
-  } else {
-    return;
-  }
-});
-
-const openModal = (modal) => {
-  document.body.style.overflow = "hidden";
-  modal.setAttribute("open", "true");
-  document.addEventListener("keydown", escClose);
-  let overlay = document.createElement("div");
-  overlay.id = "modal-overlay";
-  document.body.appendChild(overlay);
-};
-
-const closeModal = (modal) => {
-  document.body.style.overflow = "auto";
-  modal.removeAttribute("open");
-  document.removeEventListener("keydown", escClose);
-  document.body.removeChild(document.getElementById("modal-overlay"));
-};
-
-const escClose = (e) => {
-  if (e.keyCode == 27) {
-    closeModal();
-  }
-};
